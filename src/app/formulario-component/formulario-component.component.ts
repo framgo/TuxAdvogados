1fb103057d6616f0568a65736702d0a4e0736146
framgo <lucasaraujooks@gmail.com>
2023-03-23 13:14:46 -0300

    trabalhando com formulario data driven

--- src/app/formulario-component/formulario-component.component.ts
+++ src/app/formulario-component/formulario-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-formulario-component',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario-component.component.css']
 })
 export class FormularioComponentComponent {
+
+  formDataDriven!: FormGroup;
+  clienteCollection!: AngularFirestoreCollection
+
+  constructor(private clientes: FormBuilder, private register: AngularFirestore){
+    this.validaForm();
+
+    this.clienteCollection = register.collection("clientes")
+  }
+
+  validaForm(){
+    this.formDataDriven = this.clientes.group({
+      nome: ['',[Validators.required, Validators.minLength(5)]],
+      sexo: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.maxLength(255)]],
+      cidade: ['', [Validators.required, Validators.maxLength(255)]]
+    })
+  }
+
+  formData(){
+    this.clienteCollection.add(this.formDataDriven.value);
+  }
 
 }

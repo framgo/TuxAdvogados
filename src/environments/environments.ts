288cd4b1a26e0c3d6c705ab205ce0182136d2fd7
framgo <lucasaraujooks@gmail.com>
2023-03-23 13:16:08 -0300

    pasta environmets firebase

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBBXZxTRjH_PTC1KXcNfkTLbNFH7EKDjpA",
+      authDomain: "tuxadvogados.firebaseapp.com",
+      projectId: "tuxadvogados",
+      storageBucket: "tuxadvogados.appspot.com",
+      messagingSenderId: "564054450672",
+      appId: "1:564054450672:web:42cddaa21fa1572b6fbd35"
+    }
+  };
